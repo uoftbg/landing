04230cd2978b396cdd77446bb95130a746c0b089
galacticglum <nerftactic09@gmail.com>
2022-10-10 01:22:20 -0400

    add fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,33 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import("tailwindcss").Config} */
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "uoftbg-purple": {
+          // uoftbg branding colours
+          "light": "#6440A4",
+          "medium": "#4B307B",
+          "dark": "#322052",
+          "darker": "#1A0F2A",
+          "darkest": "#060312",
+        },
+        "uoftbg-orange": {
+          "light": "#D07400",
+          "medium": "#A75700",
+          "dark": "#703A00",
+        },
+      },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+      }
+    }
   },
-  plugins: [],
+  plugins: [
+    require('tailwindcss-opentype'),
+  ],
+  'darkMode': 'class',
 }
