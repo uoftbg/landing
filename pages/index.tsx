bfaefc2979a121a4f3479f5e4637f6d2bdc96e06
GalacticGlum <nerftactic09@gmail.com>
2022-10-23 11:17:54 -0400

    saving work

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,54 +3,106 @@ import Head from "next/head";
 import React from "react";
 import ThemeSwitcher from "../components/theme_switcher";
 
-const SHAPES: any = [
-  { background: "#b0bec5", zIndex: 2 },
-  { background: "#f5f5f5", zIndex: 2 },
-  { background: "#9b5de5", zIndex: 1 },
-  { background: "#f15bb5", zIndex: 2 },
-  { background: "#fee440", zIndex: 2 },
-  { background: "#00bbf9", zIndex: 2 },
-  { background: "#00f5d4", zIndex: 2 },
-];
+/**
+ * A shape.
+ */
+class Shape {
+  background: string;
+  zIndex: number;
+  style: object = {};
 
-const CONFIGURATIONS: any = [
-  {
-    positions: [
-      { left: "0%", top: "0%", height: "50%", width: "20%" },
-      { left: "20%", top: "0%", height: "50%", width: "30%" },
-      { left: "50%", top: "0%", height: "100%", width: "50%" },
-      { left: "0%", top: "50%", height: "50%", width: "30%" },
-      { left: "30%", top: "50%", height: "50%", width: "20%" },
-      { left: "70%", top: "50%", height: "50%", width: "30%" },
-      { left: "85%", top: "75%", height: "25%", width: "15%" },
-    ],
-  },
-  {
-    positions: [
-      { left: "25%", top: "20%", height: "80%", width: "15%" },
-      { left: "40%", top: "20%", height: "50%", width: "10%" },
-      { left: "50%", top: "0%", height: "100%", width: "25%" },
-      { left: "0%", top: "0%", height: "50%", width: "10%" },
-      { left: "10%", top: "0%", height: "70%", width: "15%" },
-      { left: "75%", top: "10%", height: "80%", width: "15%" },
-      { left: "90%", top: "40%", height: "60%", width: "10%" },
-    ],
-  },
-  {
-    positions: [
-      { left: "0%", top: "16.5%", height: "32%", width: "20%" },
-      { left: "20%", top: "2.7%", height: "55%", width: "34%" },
-      { left: "38%", top: "0%", height: "100%", width: "62%" },
-      { left: "0%", top: "47.3%", height: "55%", width: "34%" },
-      { left: "34%", top: "56.4%", height: "32%", width: "20%" },
-      { left: "65%", top: "45%", height: "55%", width: "34%" },
-      { left: "80%", top: "68%", height: "32%", width: "20%" },
-    ],
-  },
-];
+  /**
+   * The constructor for the Shape class.
+   * @param background The background color of the shape. Should be a valid CSS color.
+   * @param zIndex The z-index of the shape.
+   * @param style Extra styles to apply to the shape (optional). Use this to specify position, size, etc.
+   */
+  constructor(background: string, zIndex: number, style?: object = {}) {
+    this.background = background;
+    this.zIndex = zIndex;
+    this.style = style;
+  }
+}
+
+/**
+ * Build a grid of shapes for the given animation phase.
+ * @param wrapperSize The size of the wrapper div containing the shapes, in pixels.
+ * @returns An array of Shape objects.
+ */
+const buildShapes = (
+  wrapperSize: { width: number; height: number },
+  phase: number
+): Shape[] => {
+  let shapes: Shape[] = [
+    new Shape("#FFFFFF", 2), // white
+    new Shape("#8A7F1C", 2), // green
+    new Shape("#FFD2BF", 1), // beige
+    new Shape("#FFB400", 2), // yellow
+    new Shape("#FE5100", 2), // orange
+    new Shape("#F47F9B", 2), // pink
+    new Shape("#A9A9BE", 2), // blue
+  ];
+
+  const size = wrapperSize.width / 4;
+  switch (phase) {
+    case 0:
+      shapes[0].style = {
+        left: "0%",
+        top: "50%",
+        height: "50%",
+        width: `${size}px`,
+      };
+      shapes[1].style = {
+        left: "25%",
+        top: "50%",
+        height: "50%",
+        width: "25%",
+      };
+      shapes[2].style = {
+        left: "50%",
+        top: "50%",
+        height: "50%",
+        width: "50%",
+      };
+      shapes[3].style = {
+        left: "50%",
+        top: "50%",
+        height: "25%",
+        width: "50%",
+      };
+      shapes[4].style = {
+        left: "50%",
+        top: "75%",
+        height: "25%",
+        width: "50%",
+      };
+      shapes[5].style = {
+        left: "50%",
+        top: "10%",
+        height: "40%",
+        width: "25%",
+      };
+      shapes[6].style = {
+        left: "75%",
+        top: "10%",
+        height: "40%",
+        width: "25%",
+      };
+  }
+
+  return shapes;
+};
 
 const ROUNDNESS: any = [
-  { borderRadius: "6rem" },
+  [
+    { borderBottomLeftRadius: "10rem" },
+    { borderRadius: "20rem" },
+    { borderTopRightRadius: "12rem" },
+    { borderTopRightRadius: "10rem", borderBottomRightRadius: "10rem" },
+    { borderBottomLeftRadius: "10rem" },
+    { borderRadius: "16rem" },
+    { borderTopLeftRadius: "10rem" },
+  ],
   { borderRadius: "0rem" },
   { borderRadius: "30rem" },
   [
@@ -82,19 +134,43 @@ const Home: NextPage = () => {
   const [roundnessIndex, setRoundnessIndex] = React.useState(0);
 
   // Randomly select a configuration and roundness
+  // React.useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     // Get a random combination
+  //     const combination =
+  //       COMBINATIONS[randomInteger(0, COMBINATIONS.length - 1)];
+
+  //     // Set the configuration and roundness
+  //     setConfigurationIndex(combination.configuration);
+  //     setRoundnessIndex(combination.roundness);
+  //   }, 3000);
+
+  //   return () => clearInterval(interval);
+  // }, []);
+
+  // Update the size of the shapes based on size of the wrapper div
+  const [_, setWrapperSize] = React.useState({ width: 0, height: 0 });
+  const wrapperRef = React.useRef<HTMLDivElement>(null);
   React.useEffect(() => {
-    const interval = setInterval(() => {
-      // Get a random combination
-      const combination =
-        COMBINATIONS[randomInteger(0, COMBINATIONS.length - 1)];
+    const handleResize = () => {
+      const wrapper = wrapperRef.current;
+      if (wrapper) {
+        setWrapperSize({
+          width: wrapper.offsetWidth,
+          height: wrapper.offsetHeight,
+        });
+        console.log(wrapper.offsetWidth, wrapper.offsetHeight);
+      } else {
+        console.error("Wrapper ref is null");
+        setWrapperSize({ width: 0, height: 0 });
+      }
+    };
 
-      // Set the configuration and roundness
-      setConfigurationIndex(combination.configuration);
-      setRoundnessIndex(combination.roundness);
-    }, 3000);
-
-    return () => clearInterval(interval);
-  }, []);
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [wrapperRef]);
 
   return (
     <React.Fragment>
@@ -110,7 +186,11 @@ const Home: NextPage = () => {
       <ThemeSwitcher />
 
       <div className="min-w-screen min-h-screen overflow-hidden bg-black grid place-items-center m-0">
-        <main className="relative aspect-golden" style={{ width: "90vmin" }}>
+        <main
+          className="relative aspect-golden"
+          style={{ width: "90vmin" }}
+          ref={wrapperRef}
+        >
           {SHAPES.map((shape: any, index: number) => {
             // If ROUNDNESS[roudnessIndex] is an array, then it should be of length SHAPES.length.
             // In which case, we use the index of the shape to determine the roundness.
