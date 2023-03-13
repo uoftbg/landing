345625eef8f0a980ff49f0ec09c6d325be7a6a01
galacticglum <nerftactic09@gmail.com>
2023-03-13 12:00:14 -0400

    remove asset prefix for gh actions

--- next.config.js
+++ next.config.js
@@ -1,16 +1,3 @@
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-
-let assetPrefix = '';
-let basePath = '/';
-
-if (isGithubActions) {
-  // trim off `<owner>/`
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
-
-  assetPrefix = `/${repo}/`;
-  basePath = `/${repo}`;
-}
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -26,7 +13,5 @@ const nextConfig = {
 }
 
 module.exports = {
-  ...nextConfig,
-  assetPrefix: assetPrefix,
-  basePath: basePath
+  ...nextConfig
 }
